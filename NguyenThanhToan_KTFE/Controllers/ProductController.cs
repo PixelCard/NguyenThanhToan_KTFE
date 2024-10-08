47c3e2d10733b79f63bb384ab731cdb73ae340c2
Thanh <nguyenthanhtoan8544@gmail.com>
2024-10-08 20:31:25 +0700

    Add project files.

---
+++ NguyenThanhToan_KTFE/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenThanhToan_KTFE.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ NguyenThanhToan_KTFE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenThanhToan_KTFE.MvcApplication" Language="C#" %>
